6dd3809336c0a46e2cadf21b50b6fe4f881f63fb
Jake <justinejake16@gmail.com>
2026-09-07 07:35:51 +0800

    complete Exercise 5 odd numbers list comprehension with len verificatio

---
+++ Module-03/Exercise5.py
@@ -0,0 +1,6 @@
+# Complete the Statement (List comprehension using range(1, 51) with an odd condition)
+odd_numbers = [x for x in range(1, 51) if x % 2 != 0]
+print(odd_numbers)
+
+print("\nTotal count of odd numbers:", len(odd_numbers))
+
